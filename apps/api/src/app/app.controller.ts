3c7d5b92527891a4d449eb940e953b9db73ac570
Kate Sky <kateskytech@gmail.com>
2022-02-07 12:48:28 -0600

    added API moc

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Req } from '@nestjs/common';
 
 import { Message } from '@ends/api-interfaces';
 
@@ -11,5 +11,19 @@ export class AppController {
   @Get('hello')
   getData(): Message {
     return this.appService.getData();
+  }
+
+  @Get('tax/:code/:zipCode/:productSku')
+  //http://localhost:3333/api/tax/123/60032/asdasd
+  getTax(@Param() code: string, @Param() zipCode:number, @Param() productSku: string): number {
+    console.log({code}, {zipCode}, {productSku});
+
+    return 15.99;
+  }
+
+  @Get(':id')
+  findOne(@Param() params): string {
+    console.log(params.id);
+    return `This action returns a #${params.id}`;
   }
 }
