346e7a4863c9a0b0ce77a057802abfd16cc10982
Kate Sky <kateskytech@gmail.com>
2022-02-14 16:49:50 -0600

    added swagger

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -8,15 +8,27 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
 
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
-  await app.listen(port);
+  //await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
   );
+  const config = new DocumentBuilder()
+  .setTitle('Tax API example')
+  .setDescription('The tax API description')
+  .setVersion('1.0')
+  .addTag('ends-tax')
+  .build();
+const document = SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('api', app, document);
+
+await app.listen(port);
 }
 
 bootstrap();
