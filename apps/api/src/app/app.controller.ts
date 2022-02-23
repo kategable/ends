d868f13e6e51c33b9bb3464461fada73af7f35ff
Kate Sky <kateskytech@gmail.com>
2022-02-23 09:35:58 -0600

    fixed swagger

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -3,6 +3,7 @@ import { Body, Controller, Get, Param, Post, Req } from '@nestjs/common';
 import { CartRequest, Message } from '@ends/api-interfaces';
 
 import { AppService } from './app.service';
+import { ApiBody,ApiOperation } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
@@ -12,14 +13,12 @@ export class AppController {
   getData(): Message {
     return this.appService.getData();
   }
-
-
-
+  @ApiBody({ type: [CartRequest], description: "Returns ends-tax amount for a cart" })
   @Post("calculate")
   calculate(@Body() cartRequest: CartRequest) {
     return this.appService.culculate(cartRequest);
   }
-
+  @ApiBody({type: [CartRequest],description: "Save order to ends"})
   @Post("save")
   saveOrder(@Body() cartRequest: CartRequest) {
     return this.appService.saveOrder(cartRequest);
--- libs/api-interfaces/src/lib/api-interfaces.ts
+++ libs/api-interfaces/src/lib/api-interfaces.ts
@@ -1,20 +1,34 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export interface Message {
   message: string;
 }
-export interface CartRequest
-{
+
+export class CartRequest  {
+  @ApiProperty({
+    example: 'adasd-asd-asd-asd',
+    description: 'Registered ShopId',
+  })
   shopId: string;
+  @ApiProperty({
+    example: { product: '123', price: 12.99, quanity:1 } as Cart,
+    description: 'Cart details',
+  })
   cart: Cart;
+  @ApiProperty({
+    example: { state: 'IL', zipCode: '60030' } as Shipping,
+    description: 'Shipping details',
+  })
   shipping: Shipping;
+  @ApiProperty()
   total: number;
-  tax: number;
 }
-export interface Cart{
-  product:string;
-  price:number;
+export interface Cart {
+  product: string;
+  price: number;
   quanity: number;
 }
-export interface Shipping{
+export interface Shipping {
   addressLine1: string;
   addressLine2: string;
   city: string;
