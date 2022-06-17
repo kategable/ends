29e9c504f052ffe8771acd750e72d7d59153d2ed
Kate Sky <kateskytech@gmail.com>
2022-06-17 09:13:50 -0500

    adding material

--- apps/calculator-app/src/app/app.module.ts
+++ apps/calculator-app/src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
-import { HttpClientModule } from '@angular/common/http';
 import { ShopComponent } from './shop/shop.component';
-
+import { MaterialModule } from './material.module';
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent, ShopComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
